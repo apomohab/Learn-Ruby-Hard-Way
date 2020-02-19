ba7f3c52a7cae39213b240dde2a58e60eff64d3a
Udemy course ruby <aposara.acp@gmail.com>
2020-02-19 21:16:02 +0300

    numerss and math

---
+++ Numbers_Math.rb
@@ -0,0 +1,30 @@
+#this Drills for learning how to used numbers and math in ruby
+
+puts "i will now count my chicknes"
+
+puts "Hens #{25 + 30 / 6}"
+
+puts "Rana #{100 - 25 * 3 % 4}"
+
+puts "Now i will count my eggs : "
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "it is true thst 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "what is 3 + 2 ? #{3 + 2}"
+
+puts "what is 5 - 7 #{5 - 7 }"
+
+puts "oh, that's  why it's flase"
+
+puts "how about some more ."
+
+puts "is it greater ? #{5 > -2}"
+
+puts "is it greater or equal ? #{5 >= 2}"
+
+puts "is it less or equal #{5 <= -2}"
+
